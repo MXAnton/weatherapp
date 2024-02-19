9d8f3cb3af6eb340c9de500c832623e0d77061ec
Anton Lehrberg <lehrberg.anton@gmail.com>
2024-02-19 21:15:12 +0100

    Refactor code.

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-const OPENWEATHERMAPAPIKEY = '';
-const GOOGLEMAPSAPIKEY = '';
-
+const OPEN_WEATHERMAP_API_KEY = "";
 
 const search = document.getElementById("weather-data-search");
 const searchGlass = document.getElementById("weather-data-search__glass");
-const place = document.getElementById("place");
 
+const placeElement = document.getElementById("place");
+const placeNameElement = placeElement.querySelector(".place__name");
+const placeCountryElement = placeElement.querySelector(".place__country");
+const placeCountryFlagElement = placeElement.querySelector(
+  ".place__country-flag"
+);
+const placeLatElement = placeElement.querySelector(".place__lat");
+const placeLonElement = placeElement.querySelector(".place__lon");
 
 // Search place and weather on enter press in searchinput
 search.addEventListener("keypress", function (event) {
@@ -15,11 +20,8 @@ search.addEventListener("keypress", function (event) {
   }
 });
 
-
-
 let marker;
 let map, infoWindow;
-window.initMap = initMap;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -32,13 +34,15 @@ function initMap() {
   marker = new google.maps.Marker({
     position: { lat: 0, lng: 0 },
     map: map,
-    zIndex: Math.round(0 * -100000) << 5
+    zIndex: Math.round(0 * -100000) << 5,
   });
 }
+window.initMap = initMap;
+
 function setMapPos(_lat, _lon) {
   const pos = {
     lat: parseFloat(_lat),
-    lng: parseFloat(_lon)
+    lng: parseFloat(_lon),
   };
 
   map.setZoom(10);
@@ -51,109 +55,142 @@ function setMapPos(_lat, _lon) {
   marker.setPosition(pos);
 }
 
-
-function searchPlace() {
+async function searchPlace() {
   const _city = search.value.trim();
-  if (_city == null || _city == '') {
-    alert('Input city...');
-    search.value = '';
+  if (_city == null || _city == "") {
+    alert("Input city...");
+    search.value = "";
     search.focus();
     return;
   }
 
   // Animate search input magnifying glass
-  searchGlass.animate([
+  searchGlass.animate(
+    [
+      {
+        transform: "rotate(0)",
+      },
+      {
+        transform: "rotate(359deg)",
+      },
+      {
+        transform: "rotate(720deg)",
+      },
+    ],
     {
-      transform: 'rotate(0)'
-    },
-    {
-      transform: 'rotate(359deg)'
-    },
-    {
-      transform: 'rotate(720deg)'
-    }
-  ], 
-  {
-    duration: 300,
-    iterations: 1,
-  });
-
-  // SET LAT & LON BASED ON CITYinput VIA GEOAPI
-  let _lat = 0;
-  let _lon = 0;
-
-  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${_city}&limit=1&appid=${OPENWEATHERMAPAPIKEY}`)
-    .then(res => res.json())
-    .then(json => {
-      _lat = json[0].lat;
-      _lon = json[0].lon;
-
-      // Show position on maps
-      setMapPos(_lat, _lon);
-
-      place.querySelector('.place__name').textContent = json[0].name;
-      place.querySelector('.place__country').textContent = json[0].country;
-      place.querySelector('.place__country-flag').src = ''; // Raise old first, new can take time to load
-      place.querySelector('.place__country-flag').src = `https://flagsapi.com/${json[0].country}/flat/32.png`;
-      place.querySelector('.place__lat').textContent = _lat;
-      place.querySelector('.place__lon').textContent = _lon;
-
-      animatePlaceTexts();
-
-      searchWeather(_lat, _lon);
-    })
-    .catch (error => {
-      console.warn('Could not find data. Error: ' + error);
-      alert('Could not find data for: ' + _city);
-    });
-    
-    search.value = '';
-    search.focus();
-}
-
-function searchWeather(_lat, _lon) {
-  // GET WEATHER
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${_lat}&lon=${_lon}&appid=${OPENWEATHERMAPAPIKEY}&units=metric`
-  ).then(res => res.json()).then(
-    json => {
-      const _weatherCard = document.querySelector(".weather-card");
-      const _weather = _weatherCard.querySelector(".weather-card__weather");
-      const _temp = _weatherCard.querySelector(".weather-card__temp");
-      const _feelsLike = _weatherCard.querySelector(".weather-card__feels-like");
-      const _humidity = _weatherCard.querySelector(".weather-card__humidity");
-      const _rain = _weatherCard.querySelector(".weather-card__rain");
-      const _wind = _weatherCard.querySelector(".weather-card__wind");
-
-      _weather.textContent = `${json.weather[0].main}, ${json.weather[0].description}`;
-      _temp.textContent = `${parseInt(json.main.temp)} °C`;
-      _feelsLike.textContent = `${parseInt(json.main.feels_like)} °C`;
-      _humidity.textContent = `${parseInt(json.main.humidity)} %`;
-      try {
-        _rain.parentElement.style.display = 'block';
-        _rain.textContent = `${parseInt(json.rain['1h'])} mm last hour`;
-      } catch {
-        _rain.parentElement.style.display = 'none';
-        console.warn('No rain data available.');
-      }
-      _wind.textContent = `${parseInt(json.wind.speed)} m/s`;
+      duration: 300,
+      iterations: 1,
     }
   );
+
+  // SET LAT & LON BASED ON CITYinput VIA GEOAPI
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${_city}&limit=1&appid=${OPEN_WEATHERMAP_API_KEY}`
+  )
+    .then((_res) => {
+      return _res.json();
+    })
+    .catch((error) => {
+      console.warn("Could not find data. Error: " + error);
+      alert("Could not find data for: " + _city);
+      return "ERROR";
+    });
+  if (response === "ERROR" || response.cod || response.message) {
+    return;
+  }
+
+  const place = response[0];
+
+  const lat = place.lat;
+  const lon = place.lon;
+
+  // Show position on maps
+  setMapPos(lat, lon);
+
+  placeNameElement.textContent = place.name;
+  placeCountryElement.textContent = place.country;
+  placeCountryFlagElement.src = ""; // Raise old first, new can take time to load
+  placeCountryFlagElement.src = `https://flagsapi.com/${place.country}/flat/32.png`;
+  placeLatElement.textContent = lat;
+  placeLonElement.textContent = lon;
+
+  animatePlaceTexts();
+
+  searchWeather(lat, lon);
+
+  search.value = "";
+  search.focus();
 }
 
+async function searchWeather(_lat, _lon) {
+  // GET WEATHER
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${_lat}&lon=${_lon}&appid=${OPEN_WEATHERMAP_API_KEY}&units=metric`
+  )
+    .then((_res) => {
+      return _res.json();
+    })
+    .catch((error) => {
+      console.warn("Could not find data. Error: " + error);
+      alert("Could not find data for: " + _city);
+      return "ERROR";
+    });
+  if (response === "ERROR" || (response.cod && response.message)) {
+    return;
+  }
 
-const placeTextsScaleUp = [
-  { transform: 'scale(0)' },
-  { transform: 'scale(1)' }
-];
-const placeTextsTiming = {
-  duration: 300,
-  iterations: 1,
+  const weatherCardElement = document.querySelector(".weather-card");
+  const weatherElement = weatherCardElement.querySelector(
+    ".weather-card__weather"
+  );
+  const tempElement = weatherCardElement.querySelector(".weather-card__temp");
+  const feelsLikeElement = weatherCardElement.querySelector(
+    ".weather-card__feels-like"
+  );
+  const humidityElement = weatherCardElement.querySelector(
+    ".weather-card__humidity"
+  );
+  const rainElement = weatherCardElement.querySelector(".weather-card__rain");
+  const windElement = weatherCardElement.querySelector(".weather-card__wind");
+
+  weatherElement.textContent = `${response.weather[0].main}, ${response.weather[0].description}`;
+  tempElement.textContent = `${parseInt(response.main.temp)} °C`;
+  feelsLikeElement.textContent = `${parseInt(response.main.feels_like)} °C`;
+  humidityElement.textContent = `${parseInt(response.main.humidity)} %`;
+  console.log(response);
+  try {
+    rainElement.parentElement.style.display = "block";
+    rainElement.textContent = `${parseInt(response.rain["1h"])} mm last hour`;
+  } catch {
+    rainElement.parentElement.style.display = "none";
+    console.warn("No rain data available.");
+  }
+  windElement.textContent = `${parseInt(response.wind.speed)} m/s`;
 }
+
 function animatePlaceTexts() {
-  place.querySelector('.place__name').animate(placeTextsScaleUp, placeTextsTiming);
-  place.querySelector('.place__country').animate(placeTextsScaleUp, placeTextsTiming);
-  place.querySelector('.place__country-flag').animate(placeTextsScaleUp, placeTextsTiming);
-  place.querySelector('.place__lat').animate(placeTextsScaleUp, placeTextsTiming);
-  place.querySelector('.place__lon').animate(placeTextsScaleUp, placeTextsTiming);
+  const placeTextsScaleUp = [
+    { transform: "scale(0)" },
+    { transform: "scale(1)" },
+  ];
+  const placeTextsTiming = {
+    duration: 300,
+    iterations: 1,
+  };
+
+  placeElement
+    .querySelector(".place__name")
+    .animate(placeTextsScaleUp, placeTextsTiming);
+  placeElement
+    .querySelector(".place__country")
+    .animate(placeTextsScaleUp, placeTextsTiming);
+  placeElement
+    .querySelector(".place__country-flag")
+    .animate(placeTextsScaleUp, placeTextsTiming);
+  placeElement
+    .querySelector(".place__lat")
+    .animate(placeTextsScaleUp, placeTextsTiming);
+  placeElement
+    .querySelector(".place__lon")
+    .animate(placeTextsScaleUp, placeTextsTiming);
 }
